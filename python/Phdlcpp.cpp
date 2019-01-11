f9c00e2af1647fde38b27efecbf00cb761cd492e
Lars Timm <larsdtimm@gmail.com>
2019-01-11 15:49:04 +0100

    Added gil_scoped_release to Hdlcpp read and write methods. (#4)
    This enables the usage of a python thread for doing reads, while writing
    with another.

--- python/Phdlcpp.cpp
+++ python/Phdlcpp.cpp
@@ -31,8 +31,8 @@ PYBIND11_MODULE(phdlcpp, m)
                 bytes = 0;
 
             return pybind11::bytes(reinterpret_cast<char *>(data), bytes);
-        })
+        }, pybind11::call_guard<pybind11::gil_scoped_release>())
         .def("write", [](Hdlcpp::Hdlcpp *hdlcpp, char *data, uint16_t length) {
             return hdlcpp->write(reinterpret_cast<uint8_t *>(data), length);
-        });
+        }, pybind11::call_guard<pybind11::gil_scoped_release>());
 }
