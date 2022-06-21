0c235b66d50614d5775cc1e212c9babbcf4a3928
Hans Binderup <habi@bang-olufsen.dk>
2022-06-21 17:26:48 +0200

    Correct docker script + add ssh to dockerfile script

--- scripts/build_buildenv.sh
+++ scripts/build_buildenv.sh
@@ -3,10 +3,9 @@ set -e
 
 DOCKER_IMAGE=$(grep -A1 container: .github/workflows/build.yml |grep image: | xargs | cut -d ' ' -f2)
 
-if [ "$1" != "" ]; then
-    DIGIT=$(echo "$DOCKER_IMAGE" | cut -d '.' -f2)
-    DOCKER_IMAGE=$(echo "$DOCKER_IMAGE" | cut -d '.' -f1).$((DIGIT + 1))
-fi
+# Increment current version to avoid override
+DIGIT=$(echo "$DOCKER_IMAGE" | cut -d '.' -f2)
+DOCKER_IMAGE=$(echo "$DOCKER_IMAGE" | cut -d '.' -f1).$((DIGIT + 1))
 
 docker build --no-cache -t "$DOCKER_IMAGE" scripts
 
