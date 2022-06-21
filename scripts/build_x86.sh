6683af6793f11838674015062f41b12a83f3faee
Hans Binderup <habi@bang-olufsen.dk>
2022-06-21 15:36:37 +0200

    Minor clean up

--- scripts/build_x86.sh
+++ scripts/build_x86.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 set -e
 
-# mkdir -p .build-external; pushd .build-external
-# make -j "$(nproc)"
-# popd
-
 mkdir -p .build-x86; pushd .build-x86
 cmake --no-warn-unused-cli -DBUILD_EXTERNAL=1 -DCMAKE_TOOLCHAIN_FILE=cmake/gcc.cmake ..;
 make -j "$(nproc)"
