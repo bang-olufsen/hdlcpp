ce99f647abbcdf84b7b8662b7ad1960169160467
Anders Bjørn Nedergaard <ape.anp@gmail.com>
2022-07-04 13:48:06 +0200

    Docker update (#34)
    * add gdb to container + changed source dir for docker build
    
    * bumped docker image

--- scripts/build_buildenv.sh
+++ scripts/build_buildenv.sh
@@ -2,7 +2,7 @@
 set -e
 
 DOCKER_IMAGE=$(scripts/docker_image.sh 1)
-docker build --no-cache -t "$DOCKER_IMAGE" docker
+docker build --no-cache -t "$DOCKER_IMAGE" scripts
 
 # Uncomment below to push the image
 #docker push "$DOCKER_IMAGE"
