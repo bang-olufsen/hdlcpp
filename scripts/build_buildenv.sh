f7dd9d756e3191048bb985e86a1640807b8b7b2a
anedergaard <ape.anp@gmail.com>
2022-07-04 13:38:01 +0200

    add gdb to container + changed source dir for docker build

--- scripts/build_buildenv.sh
+++ scripts/build_buildenv.sh
@@ -2,7 +2,8 @@
 set -e
 
 DOCKER_IMAGE=$(scripts/docker_image.sh 1)
-docker build --no-cache -t "$DOCKER_IMAGE" docker
+echo "docker image $DOCKER_IMAGE"
+docker build --no-cache -t "$DOCKER_IMAGE" scripts
 
 # Uncomment below to push the image
 #docker push "$DOCKER_IMAGE"
